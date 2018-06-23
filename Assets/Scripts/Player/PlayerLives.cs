25604cbca8f40f1f9e460f704bfb7414feb1f70e
Marcos Righi Brasil <marcos_righi@yahoo.com.br>
2018-06-23 14:51:30 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Player/PlayerLives.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLives : MonoBehaviour {
+
+    [Range (1, 15)]
+    public int totalLives = 3;
+
+    public int currentLivesCount { get; private set; }
+
+    private void Awake () {
+        currentLivesCount = totalLives;
+    }
+
+    public void AddLife () {
+        currentLivesCount++;
+    }
+
+    public void SubtractLife () {
+        currentLivesCount--;
+    }
+
+}
---
+++ Assets/_Marcos/Scripts/IngameUI/PauseScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScreen : MonoBehaviour {
+
+    public GameObject pauseCanvas;
+    public Button resumeButton;
+    public Button menuButton;
+    
+    private void Awake () {
+        ShowPauseScreen (false);
+    }
+
+    public void ShowPauseScreen (bool value) {
+        if (value) {
+            pauseCanvas.SetActive (true);
+            resumeButton.Select ();
+        } else {
+            pauseCanvas.SetActive (false);
+            menuButton.Select ();
+        }
+    }
+
+
+}
