207f9aca4a6628f85eb04c894598ef613d2abedb
Ciro Perez Jr <ciro.perezjr@gmail.com>
2018-06-23 16:59:22 -0300

    Merge Ciro Macros
    Animações funcionando

---
+++ Assets/Scripts/SpriteController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour {
+
+    private Player player;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spriteRenderer.flipX = !player.facingRight;
+        anim.SetBool("moving", player.moving);
+	}
+}
