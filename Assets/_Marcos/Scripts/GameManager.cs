61a24f4a9d62c2fe720998bbf2fcf5b6f74e7214
Marcos Righi Brasil <marcos_righi@yahoo.com.br>
2018-06-23 14:52:50 -0300

    Tela de pause ingame + reiniciar nível

--- Assets/_Marcos/Scripts/GameManager.cs
+++ Assets/_Marcos/Scripts/GameManager.cs
@@ -1,44 +1,112 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public enum IngameState {
+    Running, Paused, GameOver
+}
 
 [RequireComponent (typeof (PlayerInput))]
 public class GameManager : MonoBehaviour {
 
     public GameObject disabledPlayerPrefab;
 
+    public IngameState currentGameState { get; private set; }
+
     private PlayerInput playerInput;
+    private PlayerLives lives;
+
+    private PauseScreen pauseScreen;
 
     private Player player;
 
-    public Coroutine blockPlayerCoroutine;
+    private Coroutine blockPlayerCoroutine;
+
+    private bool gamePaused = false;
 
 	void Awake () {
         playerInput = GetComponent<PlayerInput> ();
+        lives = GetComponent<PlayerLives> ();
+
+        pauseScreen = GetComponentInChildren<PauseScreen> ();
 
         player = FindObjectOfType<Player> ();
+
+        gamePaused = false;
 	}
 
-	void Update () {
-		if (blockPlayerCoroutine == null && playerInput.IsResetPressedDown ()) {
-            Vector3 playerPosition = player.position;
-            player.ResetPlayerPosition ();
-            CreateCadaver (playerPosition);
+    void Update () {
+        if (playerInput.IsPausePressedDown ()) {
+            TogglePause ();
+        }
 
-            blockPlayerCoroutine = StartCoroutine (BlockPlayerInputTemporarily ());
+		if (blockPlayerCoroutine == null && playerInput.IsResetPressedDown ()) {
+            DisableCurrentPlayer ();
         }
 	}
+
+    public void DisableCurrentPlayer () {
+        Vector3 playerPosition = player.position;
+        player.ResetPlayerPosition ();
+        CreateCadaver (playerPosition);
+
+        lives.SubtractLife ();
+        if (lives.currentLivesCount <= 0) {
+            GameOver ();
+        }
+
+        blockPlayerCoroutine = StartCoroutine (BlockPlayerInputTemporarily ());
+    }
 
     private void CreateCadaver (Vector3 position) {
         Instantiate (disabledPlayerPrefab, position, Quaternion.identity);
     }
 
     private IEnumerator BlockPlayerInputTemporarily () {
-        playerInput.blockPlayerInput = true;
-        yield return new WaitForSeconds (1f);
-        playerInput.blockPlayerInput = false;
+        playerInput.BlockPlayerInput (true);
+
+        yield return new WaitForSeconds (1);
+
+        playerInput.BlockPlayerInput (false);
 
         blockPlayerCoroutine = null;
     }
+
+    #region Ingame UI
+    public void TogglePause () {
+        if (gamePaused) {
+            UnpauseGame ();
+        } else {
+            PauseGame ();
+        }
+    }
+
+    private void PauseGame () {
+        pauseScreen.ShowPauseScreen (true);
+        Time.timeScale = 0;
+        playerInput.BlockPlayerInput (true);
+        gamePaused = true;
+    }
+
+    private void UnpauseGame () {
+        pauseScreen.ShowPauseScreen (false);
+        Time.timeScale = 1;
+        if (blockPlayerCoroutine == null) {
+            playerInput.BlockPlayerInput (false);
+        }
+        gamePaused = false;
+    }
+
+    public void RestartLevel () {
+        UnpauseGame ();
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+    }
+
+    private void GameOver () {
+
+    }
+    #endregion
 
 }
