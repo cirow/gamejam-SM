ec90790e14b5682b69381c59d8840fc34dca49f5
Juliana Brondani <juliana.rubenich@gmail.com>
2018-06-23 14:46:25 -0300

    Menu, audio, prefab clean, etc...

--- Assets/_Marcos/Scripts/GameManager.cs
+++ Assets/_Marcos/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent (typeof (PlayerInput))]
 public class GameManager : MonoBehaviour {
@@ -40,5 +41,7 @@ public class GameManager : MonoBehaviour {
 
         blockPlayerCoroutine = null;
     }
+
+	
 
 }
