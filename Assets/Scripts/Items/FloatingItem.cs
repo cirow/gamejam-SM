f732119bd58a4424be5ee26271d1150ee120fbc4
Unknown <juliana.rubenich@gmail.com>
2018-06-23 22:01:44 -0300

    Inicio cenário

--- Assets/Scripts/Items/FloatingItem.cs
+++ Assets/Scripts/Items/FloatingItem.cs
@@ -2,24 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloatingItem : Item {
+public class FloatingItem : MonoBehaviour {
 
     [Header ("Floating Settings")]
     public float groundOffset = 1;
 
-    void Awake () {
-        PositionItem ();
-    }
+	public float degreesPerSecond = 15.0f;
+	public float amplitude = 0.25f;
+	public float frequency = 1f;
 
-    private void PositionItem () {
-        RaycastHit2D[] hits = Physics2D.RaycastAll (transform.position, Vector2.down, 10);
 
-        foreach (RaycastHit2D hit in hits) {
-            if (hit.transform != transform) {
-                transform.position = hit.point + Vector2.up * groundOffset;
-                break;
-            }
-        }
-    }
+	Vector3 posOffset = new Vector3();
+	Vector3 tempPos = new Vector3();
+
+	void Start () {
+		posOffset = transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		
+		transform.Rotate(new Vector3(0, 80, 0) * Time.deltaTime);
+		// Float up/down with a Sin()
+		tempPos = posOffset;
+		tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+		transform.position = tempPos;
+
+	}
+
+
 
 }
