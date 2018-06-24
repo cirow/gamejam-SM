a52a60730550cf64264542d0ccc65d4a1d9df966
Marcos Righi Brasil <marcos_righi@yahoo.com.br>
2018-06-24 11:25:37 -0300

    Explosão do player

--- Assets/_Marcos/Scripts/Enemies/Enemy.cs
+++ Assets/_Marcos/Scripts/Enemies/Enemy.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
-    
+
+
+    private void Awake () {
+    }
+
     private void OnTriggerEnter2D (Collider2D collision) {
         if (collision.GetComponent<Player> () != null) {
-            //print ("player entrou no inimigo!");
-
             GameManager manager = FindObjectOfType<GameManager> ();
             if (manager != null) {
                 manager.DisableCurrentPlayer (false);
+            }
+        } else {
+            DisabledPlayer disabledPlayer = collision.GetComponent<DisabledPlayer> ();
+
+            if (disabledPlayer != null) {
+                disabledPlayer.Destroy ();
             }
         }
     }
