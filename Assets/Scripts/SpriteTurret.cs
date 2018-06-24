181a6b46c242f9ba54499d992852854f09ff649f
Ciro Perez Jr <ciro.perezjr@gmail.com>
2018-06-24 12:10:04 -0300

    Turret e Projectil animados
    E reescalados

---
+++ Assets/Scripts/SpriteTurret.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTurret : MonoBehaviour {
+
+    private Turret turretParent;
+
+	// Use this for initialization
+	void Start () {
+        turretParent = GetComponentInParent<Turret>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Shoot()
+    {
+        turretParent.Shoot();
+    }
+}
